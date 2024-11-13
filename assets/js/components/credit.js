7c1e82a47057aa9898b19b055e3b987de4654363
Vusal Orujov <or.vusal@gmail.com>
2024-11-13 17:55:42 +0400

    Refactor credit element rendering and formatting

--- assets/js/components/credit.js
+++ assets/js/components/credit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { __ } from '../translate';
+import { __ } from "../translate";
 
 export class CreditElement extends HTMLElement {
     static observedAttributes = [
@@ -18,6 +18,10 @@ export class CreditElement extends HTMLElement {
         super();
     }
 
+    connectedCallback() {
+        this.render();
+    }
+
     attributeChangedCallback(name, oldValue, newValue) {
         this.render();
     }
@@ -25,34 +29,46 @@ export class CreditElement extends HTMLElement {
     render() {
         let value = this.dataset.value || this.textContent;
         let format = this.getAttribute('format') || this.dataset.format || ':count';
+        let showFraction = this.getAttribute('fraction') || this.dataset.fraction;
 
-        if (value === '' || value === 'null' || isNaN(value)) {
+        if (value === '' || value === 'null' || isNaN(value) || value === null) {
             this.textContent = format.replaceAll(':count', __('Unlimited'));;
             return;
         }
 
         let lang = this.lang || document.documentElement.lang || 'en';
-        let amount = parseInt(value, 10);
+        // let amount = parseInt(value, 10);
+        let amount = parseFloat(value);
 
         let options = {
             style: 'decimal',
-            maximumFractionDigits: 0,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            trailingZeroDisplay: 'stripIfInteger'
         };
 
-        let formatter = new Intl.NumberFormat(lang, options);
-        let text = formatter.format(amount);
+        let titleFormatter = new Intl.NumberFormat(lang, options);
 
-        if (text.length < 7) {
-            this.textContent = format.replaceAll(':count', text);
-            return;
+        if (showFraction === 'false' || showFraction === '0' || showFraction === 'no' || showFraction === 'off') {
+            // Explictly hide fraction
+            options.minimumFractionDigits = 0;
+            options.maximumFractionDigits = 0;
         }
 
-        formatter = new Intl.NumberFormat(lang, { ...options, notation: 'compact', compactDisplay: 'short' });
-        this.textContent = format.replaceAll(':count', formatter.format(amount));
+        let formatter = new Intl.NumberFormat(lang, options);
+        let title = titleFormatter.format(amount);
+        let text = formatter.format(amount);
 
-        this.setAttribute('title',
-            new Intl.NumberFormat(lang, options).format(amount)
-        );
+        if (text.length >= 7) {
+            formatter = new Intl.NumberFormat(lang, { ...options, notation: 'compact', compactDisplay: 'short' });
+            text = formatter.format(amount);
+        }
+
+        this.textContent = format.replaceAll(':count', text);
+
+        if (title !== text) {
+            this.setAttribute('title', title);
+        }
     }
 
 }
